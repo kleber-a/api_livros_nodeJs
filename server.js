d8f090f8c61dc61be80b2d0d42af7b0c32b6ede3
Kleber Andrade <kleberandrade1998@hotmail.com>
2025-01-17 20:19:16 -0300

    test 4 vercel

--- server.js
+++ server.js
@@ -3,7 +3,18 @@ import app from "./src/app.js";
 
 const PORT = 3000
 
-app.listen(PORT, () => {
-    console.log('servidor escutando!');
-});
+// app.listen(PORT, () => {
+//     console.log('servidor escutando!');
+// });
 
+if (process.env.VERCEL === undefined) {
+    // Rodando localmente
+    app.listen(PORT, () => {
+      console.log(`Servidor escutando na porta ${PORT}`);
+    });
+  }
+  
+  // Exporte como função para Vercel
+  export default function (req, res) {
+    app(req, res);
+  }
