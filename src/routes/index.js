67cfb3ccc2b497d364fda0c2b5fa3d8474b65847
Kleber Andrade <kleberandrade1998@hotmail.com>
2025-01-17 20:32:35 -0300

    teste 6 vercel

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,6 +3,7 @@ import livros from "./livrosRoutes.js";
 import autores from "./autoresRoutes.js";
 
 const routes = (app) => {
+    app.use(express.json());
     app.route("/").get((req,res) => res.status(200).send("Curso de Node.js"));
 
     app.use(express.json(), livros, autores);
