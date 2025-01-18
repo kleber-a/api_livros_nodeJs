e7c18fe0c92a9178122d6723ced410bf577e8a8f
Kleber Andrade <kleberandrade1998@hotmail.com>
2025-01-17 22:19:04 -0300

    test

--- server.js
+++ server.js
@@ -4,6 +4,6 @@ import app from "./src/app.js";
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-    console.log(`Servidor rodando na porta ${PORT}`);
+    console.log(`Servidor rodando na porta ${PORT}`); 
 });
 
