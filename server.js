e7c10ad43c6dd30f961f0fbbe136f7d39354381b
Kleber Andrade <kleberandrade1998@hotmail.com>
2025-01-17 21:47:41 -0300

    test

--- server.js
+++ server.js
@@ -1,16 +1,9 @@
 import "dotenv/config"
 import app from "./src/app.js";
-const app = express();
-const PORT = 4000;
 
+const PORT = 3000
+    console.warn('aqui123')
 app.listen(PORT, () => {
     console.log('servidor escutando!');
 });
 
-app.get('/', (req, res) => {
-    res.send('Nossa API está funcionando!')
-})
-
-app.get('/test', (req, res) => {
-    res.send('Você entrou no test')
-})
