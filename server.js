27d3120d7e767c1701f7ba652a443ab5cca85fbf
Kleber Andrade <kleberandrade1998@hotmail.com>
2025-01-17 21:22:36 -0300

    local test

--- server.js
+++ server.js
@@ -1,37 +1,9 @@
 import "dotenv/config"
 import app from "./src/app.js";
 
-// const PORT = 3000
+const PORT = 3000
 
-// app.listen(PORT, () => {
-//     console.log('servidor escutando!');
-// });
-
-// if (process.env.VERCEL === undefined) {
-//     // Rodando localmente
-//     app.listen(PORT, () => {
-//       console.log(`Servidor escutando na porta ${PORT}`);
-//     });
-//   }
-  
-  // Exporte como função para Vercel
-  // const PORT = process.env.PORT || 3000;
-  // app.listen(PORT, () => {
-  //   console.log(`Server is running on port ${PORT}`);
-  // });
-
-  const express = require('express');
-const app = express();
-
-app.get('/', (req, res) => {
-  res.send('API funcionando corretamente!');
-});
-
-app.get('/livros', (req, res) => {
-  res.status(200).send("livros")
-});
-
-const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`Servidor rodando na porta ${PORT}`);
+    console.log('servidor escutando!');
 });
+
--- src/app.js
+++ src/app.js
@@ -2,28 +2,24 @@ import express from "express";
 import connectDataBase from "./config/dbConnect.js";
 import routes from "./routes/index.js";
 
+const conexao = await connectDataBase();
+
+conexao.on("error", (erro) => {
+    console.error("erro de conexão", erro)
+});
+
+conexao.once("open", () => {
+    console.log('conexão com o banco feita com sucesso')
+});
+
 const app = express();
+routes(app);
 
-async function startServer() {
-  try {
-    const conexao = await connectDataBase();
-    conexao.on("error", (erro) => console.error("erro de conexão", erro));
-    conexao.once("open", () => {
-      console.log("conexão com o banco feita com sucesso");
-    });
 
-    // Rotas do Express
-    routes(app);
-
-    // Inicia o servidor após garantir que o banco está conectado
-    app.listen(process.env.PORT || 3000, () => {
-      console.log("Servidor rodando...");
-    });
-  } catch (err) {
-    console.error("Erro ao conectar com o banco ou iniciar o servidor", err);
-  }
-}
-
-startServer();
+app.delete("/livros/:id", (req, res) => {
+    const index = buscarLivros(req.params.id);
+    livros.splice(index, 1);
+    res.status(200).send('livro removido com sucesso');
+});
 
 export default app;
